87ef8d1ad9c93cb216998ab93e6b6a294753b2af
Be3tle <aburatinshoaib@gmail.com>
2023-09-02 16:05:30 +0600

    fetched category id

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const handleCategory = async () => {
-  console.log("Hello First");
   const res = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
   );
   const data = await res.json();
-  console.log(data);
-  console.log("hello second");
+
+  const tabContainer = document.getElementById("tab-container");
+
+  const trimmedData = data.data.slice(0, 4);
+
+  trimmedData.forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <a onclick="handleLoadVids('${category.category_id}')" class="tab tabs-boxed">${category.category}</a>
+    `;
+    tabContainer.appendChild(div);
+  });
 };
+
+const handleLoadVids = async (categoryId) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+  );
+  const data = await res.json();
+  console.log(data.data);
+};
+
+handleCategory();
